41df42b3f7bdd0f63937dc6f6d6b5f2193448c03
Perkins <Thomas_J_Perkins@AMER.DELL.COM>
2017-07-25 08:16:46 -0500

    edited the req file, created an installer for pywin and edited the pyhook installer

--- build/windows.py
+++ build/windows.py
@@ -30,15 +30,44 @@ def _install_pil(verbose=False, dist_url="https://dist.plone.org/thirdparty/", p
     return subprocess.call(pip_cmd)
 
 
-def _install_pyhook(verbose=False, whl_start_name="pyHook-1.5.1-cp36-cp36m-"):
+def _install_pyhook(verbose=False, whl_start_dir=os.listdir("{}/build/reqs/whls".format(os.getcwd()))):
     """
-     install pyhook from the .whl files
+     install pyhook from the .whl files. There is an issue with Windows 8 and above
+     where the wheel files will not be supported
     """
-    full_whl = whl_start_name + "win32.whl" if _check_arc() == "32" else "win_amd64.whl"
-    full_whl_path = "{}/build/reqs/whls/{}".format(os.getcwd(), full_whl)
-    pip_cmd = ["pip", "install", full_whl_path]
+    all_whls = set()
+    os_arc = _check_arc()
     if verbose:
-        print("[d] running command: '{}'..".format(pip_cmd))
+        print("[d] searching for .whl files that are compatible with {} OS..".format(os_arc[0]))
+    for whl in whl_start_dir:
+        if os_arc == "32":
+            if "-win32.whl" in whl:
+                all_whls.add(whl)
+        else:
+            if "-win_amd64.whl" in whl:
+                all_whls.add(whl)
+    for whl in list(all_whls):
+        whl_path = "{}/build/reqs/whls/".format(os.getcwd()) + whl
+        pip_cmd = ["pip", "install", "--use-wheel", "--no-index", whl_path]
+        if verbose:
+            print("[d] running command: '{}'..".format(' '.join(pip_cmd)))
+        subprocess.call(pip_cmd)
+
+
+def _install_pywin(verbose=False, dist_url="https://sourceforge.net/projects/pywin32/files/pywin32/Build%20220/{}/download"):
+    """
+     install py2exe from the dist website, dunno if it's secure, don't really care,
+     it's gonna work.
+    """
+    exe_filenames = ("pywin32-220.win-amd64-py2.7.exe", "pywin32-220.win32-py2.7.exe")
+    arc = _check_arc()
+    if arc == "32":
+        dist_url = dist_url.format(exe_filenames[1])
+    else:
+        dist_url = dist_url.format(exe_filenames[0])
+    pip_cmd = ["pip" "install", dist_url]
+    if verbose:
+        print("[d] running command: '{}'..".format(' '.join(pip_cmd)))
     return subprocess.call(pip_cmd)
 
 
@@ -55,7 +84,8 @@ def _install_other(verbose=False, reqs_file_path="{}/build/reqs/win_requirements
 def win_main(verbose=False):
     __funcs__ = [
         _install_pyhook, _install_py2exe,
-        _install_pil, _install_other
+        _install_pil, _install_py2exe,
+        _install_other
     ]
     print("[i] installing Windows requirements..")
     for installation in __funcs__:
