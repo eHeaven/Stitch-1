5119c9fc113f625779822404c751124263e5939c
ekultek <thomas.perkins23@icloud.com>
2017-07-28 22:41:05 -0500

    patch for handle_exception_clause

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Copyright (c) 2017, Nathan Lopez
 # Stitch is under the MIT license. See the LICENSE file at the root of the project for the detailed license terms.
-
+from application.Stitch_Vars.globals import handle_exception_clause
 try:
     from application.stitch_cmd import server_main
     server_main()
